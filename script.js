079c99e8da7bb1e67dcdfdc58df2d8ef54cd8a5f
Michael <michael.barry.email@gmail.com>
2020-10-07 19:01:46 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+
+setTimeout(getData, 5000);
+
+function getData() {
+    header.innerHTML = '<img src="./download.jpg" alt="Newfoundland Dog" />';
+    title.innerHTML = 'Lorem ipsum dolor sit amet.';
+    excerpt.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam, reiciendis?';
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="User" />';
+    name.innerHTML = 'John Doe';
+    date.innerHTML = 'Oct 08, 2020';
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+    animated_bg_texts.forEach(bg => bgs.classList.remove('animated-bg-text'));
+}
